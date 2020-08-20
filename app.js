2f025acdc301a4687944ec2395d3500dd97f2604
Mackiers <67982622+Mackiers@users.noreply.github.com>
2020-08-20 19:20:59 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 // Load config
 dotenv.config({ path: './config/config.env' });
@@ -16,6 +18,10 @@ require('./config/passport')(passport);
 connectDB();
 
 const app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 // Logging
@@ -31,7 +37,8 @@ app.set('view engine', '.hbs');
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
 // Passport middleware
@@ -44,6 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 3000;
 
