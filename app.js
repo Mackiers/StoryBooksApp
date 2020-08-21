a93059951b47e4950bc653517e74f9bdb203d261
Mackiers <67982622+Mackiers@users.noreply.github.com>
2020-08-21 16:42:49 +0100

    Final commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 const passport = require('passport');
 const session = require('express-session');
+const methodOverride = require('method-override');
 const MongoStore = require('connect-mongo')(session);
 
 // Load config
@@ -23,14 +24,36 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    let method = req.body._method;
+    delete req.body._method;
+    return method;
+  };
+}));
+
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Handlebars helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
+
+
 // Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({ 
+  helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select,
+}, 
+defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -44,6 +67,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
